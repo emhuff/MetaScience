788ae588c8b8795143fd9798f9c6186b5dfa3b87
emhuff <emhuff@gmail.com>
2020-03-11 14:48:26 -0700

    fleshed out Experiment subclass for simple pendulum

--- metascience/python/experiment.py
+++ metascience/python/experiment.py
@@ -48,11 +48,15 @@ class Experiment():
     def generate_data():
         self._get_ideal_data_vector():
         self._add_noise()
-        self._add_systematics()
+        self.data_vector = self._add_systematics()
 
 
 class CMBExperiment(Experiment):
-    def __init__(self,cosmology = None, systematics_model = None, noise_model = None):
+    '''
+    This class instantiates an experiment where we:
+        - have measurements of CMB Tempterature taken at points in the sky.
+    '''
+    def __init__(self,cosmology = None, systematics_model = None, noise_model = None, experimental_parameters = None):
         super().__init__()
         self.kind = 'CMB'
         self.cosmology = cosmology
@@ -64,21 +68,42 @@ class CMBExperiment(Experiment):
         pass
 
 
-class PendulumExperiment(Experiment):
-    def __init__(self,cosmology = None, systematics_model = None, noise_model = None):
+class SimplePendulumExperiment(Experiment):
+    '''
+    This class instantiates an experiment where we:
+      - have measurements of the position of pendulum, theta, taken at certain exact times
+
+    '''
+    def __init__(self, cosmology = None, systematics_parameters = None, noise_parameters = None, experimental_parameters = None, seed=999):
         super().__init__()
         self.kind = 'pendulum'
-        self.cosmology = cosmology #params: g, T, L
-            'cosmology will be "g"'
+        self.cosmology = cosmology
+        self.time_between_measurements = experimental_parameters['time_between_measurements']
+        self.number_of_measurements = experimental_parameters['number_of_measurements']
+        self.noise_std_dev = noise_parameters['noise_std_dev']
+        self.boost_deriv = systematics_parameters['boost_deriv'] # what's a boost deriv?
+        self.seed = np.random.seed(seed=seed)
+        #"cosmology" params: g, T, L
 
     def _set_systematics(systematics_model):
         pass
 
-    def _set_noise(noise_model):
-        pass
+    def _add_noise():
+        noise_vector = self.noise_std_dev * np.random.randn(self.number_of_measurements)
+        self.data_vector = self.ideal_data_vector + noise_vector
 
-    def _get_ideal_data_vector(length):
-        theta = theta_0* np.cos(np.sqrt(constant_g/constant_l)* time)
-        return period
+    def _add_systematics():
+        # creating a 'boost' by multiplying the normal linear gradient of data/time
+        # by a boost (driving) factor
+        time_deriv = self.times - self.times[::-1] # compute the time deriv vector
+        data_deriv = self.data_vector-self.data_vector[::-1] #compute the data deriv vector
+        systematics_vector = self.boost_deriv*(data_deriv/time_deriv) # multiply the ratio of ddata/dtime by boost factor
+        return self.data_vector + systematics_vector # add systematics to data vector
 
+    def _get_ideal_data_vector():
+        #
+        self.times = np.arange(self.number_of_measurements) * self.time_between_measurements
+        theta = theta_0* np.cos(np.sqrt(constant_g/constant_l)* self.times)
+        self.ideal_data_vector = theta
 
+        return theta
--- metascience/python/interpret.py
+++ metascience/python/interpret.py
@@ -12,13 +12,13 @@ class ExperimentInterpreter(metaclass=ABCMeta):
         raise NotImplementedError
 
     @abstractmethod
-    def evaluate_posterior(parameters):
+    def evaluate_logL(parameters):
         '''
         What can we infer about the world from the provided experiment?
         Takes a cosmology, returns a log-posterior or similar.
         '''
         raise NotImplementedError
-    
+
     @abstractmethod
     def fit_model():
         '''
@@ -26,9 +26,54 @@ class ExperimentInterpreter(metaclass=ABCMeta):
         '''
         pass
 
-        @abstractmethod
+    @abstractmethod
+    def _add_systematics():
+        pass
+
+    @abstractmethod
     def elaborate_systematics():
         '''
         Fit a model. Generate a posterior.
         '''
+        pass
+
+class SimplePendulumExperimentInterpreter(ExperimentInterpreter):
+    def __init__(self, data_vector = None, experimental_parameters = None, noise_parameters= None, systematics_parameters = None ):
+
+        super().__init__()
+        self.data_vector = data_vector
+        self.covariance = np.eye(np.ones_like(self.data_vector)+noise_parameters['noise_std_dev']**2)
+
+    def _add_systematics():
+
+        return new_data_vector
+
+
+    def evaluate_logL(parameters):
+        '''
+        What can we infer about the world from the provided experiment?
+        Takes a cosmology, returns a log-posterior or similar.
+        '''
+        constant_g = parameters[0]]
+        constant_l = parameters[1]
+        model_data_vector = theta_0* np.cos(np.sqrt(constant_g/constant_l)* self.times)#() # contains parameters?
+        model_with_systematics = self._add_systematics(model_data_vector)
+
+        chisq = np.dot(model_with_systematics - self.data_vector,
+        return chisq
+
+    def fit_model():
+        '''
+        Fit a model. Generate a posterior.
+        '''
+        self.best_fit_parameters = scipy.optimize.root(evaluate_posterior,method='lm')
+
+    def elaborate_systematics():
+
+        # some conditional logic here
+        # then:
+        def _add_systematics():
+            # as a new kind of systematics function
+            pass
+
         pass
