62cde66dfaf94a4d9f0de79408b1bf87f6c04146
emhuff <emhuff@gmail.com>
2020-03-19 09:33:52 -0700

    Add damped/driven harmonic oscillator diffeq solver

--- metascience/python/experiment.py
+++ metascience/python/experiment.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
+from scipy.integrate import solve_ivp as solver
 
 class Experiment():
     def __init__(self):
@@ -74,36 +75,73 @@ class SimplePendulumExperiment(Experiment):
       - have measurements of the position of pendulum, theta, taken at certain exact times
 
     '''
-    def __init__(self, cosmology = None, systematics_parameters = None, noise_parameters = None, experimental_parameters = None, seed=999):
+    def __init__(self,
+                    cosmology_parameters = None,
+                    nuisance_parameters = None,
+                    experimental_parameters = None,
+                    noise_parameters = None,
+                    systematics_parameters = None,
+                    seed=999):
         super().__init__()
         self.kind = 'pendulum'
-        self.cosmology = cosmology
+        self.constant_g = cosmology_parameters['constant_g']
+        self.constant_l = nuisance_parameters['constant_l']
+        self.constant_theta_0 = nuisance_parameters['constant_theta_0']
+        self.constant_theta_v0 = nuisance_parameters['constant_theta_v0']
         self.time_between_measurements = experimental_parameters['time_between_measurements']
         self.number_of_measurements = experimental_parameters['number_of_measurements']
+        self.times = None
         self.noise_std_dev = noise_parameters['noise_std_dev']
         self.boost_deriv = systematics_parameters['boost_deriv'] # what's a boost deriv?
         self.seed = np.random.seed(seed=seed)
-        #"cosmology" params: g, T, L
-
-    def _set_systematics(systematics_model):
-        pass
-
-    def _add_noise():
-        noise_vector = self.noise_std_dev * np.random.randn(self.number_of_measurements)
-        self.data_vector = self.ideal_data_vector + noise_vector
-
-    def _add_systematics():
-        # creating a 'boost' by multiplying the normal linear gradient of data/time
-        # by a boost (driving) factor
-        time_deriv = self.times - self.times[::-1] # compute the time deriv vector
-        data_deriv = self.data_vector-self.data_vector[::-1] #compute the data deriv vector
-        systematics_vector = self.boost_deriv*(data_deriv/time_deriv) # multiply the ratio of ddata/dtime by boost factor
-        return self.data_vector + systematics_vector # add systematics to data vector
+        self.systematics_parameters = systematics_parameters
+        #"cosmology" params: g
+        #"astrophysical (nuisance)"  params: l
 
     def _get_ideal_data_vector():
-        #
-        self.times = np.arange(self.number_of_measurements) * self.time_between_measurements
-        theta = theta_0* np.cos(np.sqrt(constant_g/constant_l)* self.times)
-        self.ideal_data_vector = theta
+        '''
+        Generate ideal data vector from cosmology parameters, nuisance parameters, and experimental parameters
+        '''
+        g = self.constant_g
+        l = self.constant_l
 
-        return theta
+        c = self.systematics_parameters['drag_coeff']
+        A = self.systematics_parameters['driving_amp']
+        wd = self.systematics_parameters['driving_freq']
+        phid = self.systematics_parameters['driving_phase']
+
+        self.times = np.arange(self.number_of_measurements) * self.time_between_measurements
+        #theta = self.constant_theta_0 *
+        #    np.cos(np.sqrt(self.constant_g] / self.constant_l) * self.times)
+
+        def forcing_function(t):
+            return A*np.cos(wd*t+phid)
+
+        def oscillator_eqns(t,y):
+            x = y[0]
+            u = y[1]
+            xp = u
+            up = (forcing_function(t) - c*u - l*x)/g
+            return np.array([xp,up])
+
+        interval = self.number_of_measurements * self.time_between_measurements
+        solution = solver(oscillator_eqns,interval, np.array([self.theta_0,self.theta_v0]), t_eval = self.times)
+
+        return solution.y
+
+    def _add_systematics():
+        '''
+        Creating a 'boost' by multiplying the normal linear gradient of data/time
+        by a boost (driving) factor
+        '''
+        time_deriv = self.times - self.times[::-1] # compute the time deriv vector
+        data_deriv = self.data_vector - self.data_vector[::-1] #compute the data deriv vector
+        systematics_vector = self.boost_deriv * (data_deriv / time_deriv) # multiply the ratio of ddata/dtime by boost factor
+        return self.data_vector + systematics_vector # add systematics to data vector
+
+    def _add_noise():
+        '''
+        Add noise from noise parrameters and measurements
+        '''
+        noise_vector = self.noise_std_dev * np.random.randn(self.number_of_measurements)
+        self.data_vector = self.ideal_data_vector + noise_vector
--- metascience/python/interpret.py
+++ metascience/python/interpret.py
@@ -38,13 +38,24 @@ class ExperimentInterpreter(metaclass=ABCMeta):
         pass
 
 class SimplePendulumExperimentInterpreter(ExperimentInterpreter):
-    def __init__(self, data_vector = None, experimental_parameters = None, noise_parameters= None, systematics_parameters = None ):
+    def __init__(self,
+                    times = None,
+                    measured_data_vector = None,
+                    cosmology_parameters = None,
+                    nuisance_parameters = None,
+                    experimental_parameters = None,
+                    noise_parameters = None,
+                    systematics_parameters = None ):
 
         super().__init__()
-        self.data_vector = data_vector
-        self.covariance = np.eye(np.ones_like(self.data_vector)+noise_parameters['noise_std_dev']**2)
+        self.measured_data_vector = measured_data_vector
+        self.covariance = np.eye(np.ones_like(self.measured_data_vector) + noise_parameters['noise_std_dev']**2)
+        self.times = times
+        self.best_fit_parameters = None
 
-    def _add_systematics():
+    def _add_systematics(a_data_vector):
+        number_of_functions = len(self.systematics_parameters)
+        
 
         return new_data_vector
 
@@ -54,19 +65,20 @@ class SimplePendulumExperimentInterpreter(ExperimentInterpreter):
         What can we infer about the world from the provided experiment?
         Takes a cosmology, returns a log-posterior or similar.
         '''
-        constant_g = parameters[0]]
+        constant_g = parameters[0]
         constant_l = parameters[1]
-        model_data_vector = theta_0* np.cos(np.sqrt(constant_g/constant_l)* self.times)#() # contains parameters?
+        constant_theta_0 = parameters[2]
+        model_data_vector = constant_theta_0 * np.cos(np.sqrt(constant_g / constant_l) * self.times)
         model_with_systematics = self._add_systematics(model_data_vector)
 
-        chisq = np.dot(model_with_systematics - self.data_vector,
+        chisq = np.dot(model_with_systematics - self.measured_data_vector,)
         return chisq
 
     def fit_model():
         '''
         Fit a model. Generate a posterior.
         '''
-        self.best_fit_parameters = scipy.optimize.root(evaluate_posterior,method='lm')
+        self.best_fit_parameters = scipy.optimize.root(evaluate_logL posterior,method='lm')
 
     def elaborate_systematics():
 
