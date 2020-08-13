240857e9b3a3f5bc3e0a3a957b66465c5be5eda2
emhuff <emhuff@gmail.com>
2020-08-13 11:21:46 -0700

    add test_interpreter

---
+++ metascience/python/test_interpreter.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import experiment
+import interpret
+import cosmology
+import ipdb
+
+truth = cosmology.TrueCosmology()
+experimental_parameters = {'times':np.linspace(0,5,50)}
+noise_parameters = np.array([0.1])
+true_systematics_parameters = np.array([.1])
+true_parameters = truth.get_parameter_set()
+true_parameters[4] = np.sqrt(12.)
+true_parameters[2] = 0.5
+pendulum = experiment.SimplePendulumExperiment(cosmology=truth,
+                                               experimental_parameters=experimental_parameters,
+                                               cosmology_parameters=true_parameters[:truth.n_cosmological],
+                                               nuisance_parameters=true_parameters[truth.n_cosmological:],
+                                               systematics_parameters=true_systematics_parameters,
+                                               noise_parameters = noise_parameters)
+pendulum.generate_data()
+
+model = cosmology.CosineCosmology()
+#noise_parameters = np.array([0.2])
+starting_systematics_parameters = np.array([0.,])
+
+pendulum_interp = interpret.SimplePendulumExperimentInterpreter(experiment = pendulum,
+                                                                 starting_systematics_parameters = starting_systematics_parameters,
+                                                                 noise_parameters = noise_parameters,
+                                                                 prior = None,
+                                                                 cosmology = model)
+pendulum_interp.fit_model()
+print(f"fit status: {pendulum_interp.fit_status}")
+w_true = np.sqrt(truth.fiducial_cosmological_parameters[0]/truth.fiducial_cosmological_parameters[1])
+w_fit = np.sqrt(pendulum_interp.best_fit_cosmological_parameters[0]/pendulum_interp.fiducial_cosmological_parameters[0])
+
+print(f"best-fit cosmological parameters: {w_fit}")
+print(f"true cosmological parameters: {w_true}")
+
+plt.errorbar(pendulum.times,pendulum.observed_data_vector,np.zeros(pendulum.times.size)+noise_parameters,fmt='o',label='data',)
+plt.plot(pendulum.times,pendulum.ideal_data_vector,label='true (ideal)')
+plt.plot(pendulum.times,pendulum_interp.best_fit_observed_model,label='fit')
+plt.plot(pendulum.times,pendulum_interp.best_fit_ideal_model,label='ideal fit')
+plt.legend(loc='best')
+plt.show()
+
+# Show the systematics.
+
+ipdb.set_trace()
