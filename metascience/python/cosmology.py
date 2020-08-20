0f9519311fecc72c2d7d22d9856126e249f8d1b1
emhuff <emhuff@gmail.com>
2020-08-20 10:17:24 -0700

    changed oscillator cosmology to have a single frequency parameter instead of l/g

--- metascience/python/cosmology.py
+++ metascience/python/cosmology.py
@@ -87,7 +87,7 @@ class CargoCultCosmology_Tens(Cosmology):
 class StraightLineCosmology(Cosmology):
     def __init__(self):
         self.complexity = 0
-        self.n_cosmological = 2
+        self.n_cosmological = 1
         self.n_nuisance = 1
         self.n_parameters = self.n_cosmological + self.n_nuisance
         self.fiducial_cosmological_parameters = np.array([1.])
@@ -109,7 +109,7 @@ class StraightLineCosmology(Cosmology):
 class CosineCosmology(Cosmology):
     def __init__(self):
         self.complexity = 1
-        self.n_cosmological = 2
+        self.n_cosmological = 1
         self.n_nuisance = 2
         self.n_parameters =  self.n_nuisance + self.n_cosmological
         self.fiducial_cosmological_parameters = np.array([9.8,1.0])
@@ -122,13 +122,12 @@ class CosineCosmology(Cosmology):
     def generate_model_data_vector(self,times, parameters = None):
         # define model for data with parameters above
 
-        constant_g = parameters[0]
-        constant_l = parameters[1]
-        constant_theta_0 = parameters[2]
-        constant_phase = parameters[3]
+        constant_w = parameters[0]
+        constant_theta_0 = parameters[1]
+        constant_phase = parameters[2]
 
         model_data_vector = constant_theta_0 \
-        * np.cos(np.sqrt(constant_g /constant_l) * times + constant_phase)
+        * np.cos(constant_w * times + constant_phase)
         return model_data_vector
 
 
@@ -151,15 +150,13 @@ class TrueCosmology(Cosmology):
         Generate ideal data vector from cosmology parameters, nuisance
         parameters, and experimental parameters
         '''
-        g = parameters[0]
-        l = parameters[1]
-
-        c = parameters[2]
-        A = parameters[3]
-        wd = parameters[4]
-        phid = parameters[5]
-        theta_x0 = parameters[6]
-        theta_v0 = parameters[7]
+        w = parameters[0]
+        c = parameters[1]
+        A = parameters[2]
+        wd = parameters[3]
+        phid = parameters[4]
+        theta_x0 = parameters[5]
+        theta_v0 = parameters[6]
 
         #theta = self.constant_theta_0 *
         #    np.cos(np.sqrt(self.constant_g] / self.constant_l) * self.times)
@@ -177,7 +174,7 @@ class TrueCosmology(Cosmology):
             x = y[0]
             u = y[1]
             xp = u
-            up = (forcing_function(t) - c * u - g * x / l)
+            up = (forcing_function(t) - c * u - w**2 * x)
             return np.array([xp,up])
 
         # minimum and maximum times over which the solution should be calculated
--- metascience/python/test_cosmology.py
+++ metascience/python/test_cosmology.py
@@ -7,14 +7,13 @@ truth = cosmology.TrueCosmology()
 
 truth_parameters = truth.get_parameter_set()
 
-truth_parameters[0] = 9.8 # g cosmological parameter 1
-truth_parameters[1] = 1.0 # l cosmological parameter 2
-truth_parameters[2] = 0.01 # C -- drag/damping coefficient
-truth_parameters[3] = .0 # A -- amplitude of forcing function
-truth_parameters[4] = .1 # wd -- freq. of forcing function
-truth_parameters[5] = 0.0 # phid -- phase of forcing function
-truth_parameters[6] = 1.0  # theta_x0 -- initial position of pendulum
-truth_parameters[7] = 0.0 # theta_v0-- initial velocity of pendulum
+truth_parameters[0] = 1.0 # oscillator frequency
+truth_parameters[1] = 0.01 # C -- drag/damping coefficient
+truth_parameters[2] = .0 # A -- amplitude of forcing function
+truth_parameters[3] = .1 # wd -- freq. of forcing function
+truth_parameters[4] = 0.0 # phid -- phase of forcing function
+truth_parameters[5] = 1.0  # theta_x0 -- initial position of pendulum
+truth_parameters[6] = 0.0 # theta_v0-- initial velocity of pendulum
 
 # At what times are we generating the data?
 times = np.linspace(0,10,5000)
@@ -26,10 +25,9 @@ real_data = truth.generate_model_data_vector(times,parameters=truth_parameters)
 # Test the model cosmology.
 model = cosmology.CosineCosmology()
 model_parameters = model.get_parameter_set()
-model_parameters[0] = 9.8
-model_parameters[1] = 1.0
-model_parameters[2] = 1.0 # amplitude
-model_parameters[3] = 0.0 # phase
+model_parameters[0] = 1.0
+model_parameters[1] = 1.0 # amplitude
+model_parameters[2] = 0.0 # phase
 
 model_data = model.generate_model_data_vector(times,parameters=model_parameters)
 
